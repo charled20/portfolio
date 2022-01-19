4822bb8ae8bf898108e82ce5c2ada1e3411ba485
charled21 <craigtrecher@gmail.com>
2022-01-19 14:58:02 +0800

    1-19-22 2:57pm
    -added multiple css files to index

--- index.php
+++ index.php
@@ -5,19 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index Page</title>
+    <!-- #### CSS ####-->
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <link href="css/index.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- #### CSS ####-->
 </head>
 <body>
-    WELCOME TO MY PORFOLIO
+    <div class="font-text">
+    <h2 class="text-center mt-4 mb-4">MY PORFOLIO</h2>
+        <a class="link-color" href="/portfolio/pages/thesis_git/main.php">APPLICANT TRACKER WITH DSS</a>
+        <br>
+        <a class="link-color" href="#">SECOND PAGE</a>
+        <br>
+        <a class="link-color" href="#">THIRD PAGE</a>
+        <br>
+        <a class="link-color" href="#">FOURTH PAGE</a>
+        <br>
+        <a class="link-color" href="#">FIFTH PAGE</a>
+    </div>
 
-    <br>
-    <a  href="/portfolio/pages/thesis_git/main.php">APPLICANT TRACKER WITH DSS</a>
-    <br>
-    <a href="#">SECOND PAGE</a>
-    <br>
-    <a href="#">THIRD PAGE</a>
-    <br>
-    <a href="#">FOURTH PAGE</a>
-    <br>
-    <a href="#">FIFTH PAGE</a>
+    <!-- #### SCRIPTS #### -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- #### SCRIPTS #### -->
 </body>
 </html>
