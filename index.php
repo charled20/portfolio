2bd51f268f2c586118e68eaec8f31bb6e3adb886
charled21 <craigtrecher@gmail.com>
2022-01-19 17:44:09 +0800

    1-19-22 5:43pm
    -added several functionalities to index

--- index.php
+++ index.php
@@ -9,24 +9,87 @@
     <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
     <link href="css/index.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- #### CSS ####-->
 </head>
 <body>
-    <div class="font-text">
-    <h2 class="text-center mt-4 mb-4">MY PORFOLIO</h2>
-        <a class="link-color" href="/portfolio/pages/thesis_git/main.php">APPLICANT TRACKER WITH DSS</a>
-        <br>
-        <a class="link-color" href="#">SECOND PAGE</a>
-        <br>
-        <a class="link-color" href="#">THIRD PAGE</a>
-        <br>
-        <a class="link-color" href="#">FOURTH PAGE</a>
-        <br>
-        <a class="link-color" href="#">FIFTH PAGE</a>
+    <div class="container d-flex justify-content-center">
+        <div class="col-md-8 font-text">
+
+        <h2 class="text-center" style="margin-top: 40px; margin-bottom: 60px;">MY PORFOLIO</h2>
+        <hr>
+        
+
+            <div class="row d-flex justify-content-between">
+                <div class="custom-cards">
+                        <div class="container h-100">
+                            <div class="row align-items-center h-100">
+                                <div class="col-12 mx-auto">
+                                    <div class="h-100 border-primary justify-content-center">
+                                        <a id="button1" type="button" class="ml-4 mr-4 text-center link-color" href="pages/thesis_git/main.php">  
+                                        APPLICANT TRACKER WITH DSS
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                
+                </div>
+
+                <div class="custom-cards row">
+                        <div class="container h-100">
+                            <div class="row align-items-center h-100">
+                                <div class="col-12 mx-auto">
+                                    <div class="h-100 border-primary justify-content-center">
+                                        <div class="ml-4 mr-4 text-center link-color">
+                                            SECOND PAGE
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                
+                </div>
+            </div>
+
+            <div class="mt-4 row d-flex justify-content-between">
+                <div class="custom-cards">
+                        <div class="container h-100">
+                            <div class="row align-items-center h-100">
+                                <div class="col-12 mx-auto">
+                                    <div class="h-100 border-primary justify-content-center">
+                                        <div class="ml-4 mr-4 text-center link-color">
+                                            THIRD PAGE
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                
+                </div>
+
+                <div class="custom-cards row">
+                        <div class="container h-100">
+                            <div class="row align-items-center h-100">
+                                <div class="col-12 mx-auto">
+                                    <div class="h-100 border-primary justify-content-center">
+                                        <div class="ml-4 mr-4 text-center link-color">
+                                            FOURTH PAGE
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                
+                </div>
+            </div>
+        </div>
+            
+        
+        <!-- <a class="link-color text-center align-items-center" href="/portfolio/pages/thesis_git/main.php">APPLICANT TRACKER WITH DSS</a> -->
     </div>
 
+
     <!-- #### SCRIPTS #### -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>    
     <!-- #### SCRIPTS #### -->
 </body>
 </html>
